d4404ec3c22ed405767fb6ddb6bbd959aac01e1e
Diego Pino Navarro <dpino@metro.org>
2018-04-13 12:15:12 -0400

    Fix other peoples "if"

--- scripts/fcrepo.sh
+++ scripts/fcrepo.sh
@@ -24,16 +24,16 @@ else
 fi
 
 echo "Installing Fedora"
-java -jar /tmp/fcrepo-installer-3.8.1.jar "$SHARED_DIR"/configs/install.properties
+
 
 # Check the exit code from the installation process
-if [ $? -ne 0 ]; then
+if ! java -jar /tmp/fcrepo-installer-3.8.1.jar "$SHARED_DIR"/configs/install.properties
+then
   # Had a corrupt jarfile in cache, if can't install then redownload it
   echo "Problem with jar file, redownloading"
   wget -q -O "/tmp/fcrepo-installer-3.8.1.jar" "https://github.com/fcrepo3/fcrepo/releases/download/v3.8.1/fcrepo-installer-3.8.1.jar"
-  java -jar /tmp/fcrepo-installer-3.8.1.jar /tmp/install.properties
-
-  if [ $? -ne 0 ]; then
+  if ! java -jar /tmp/fcrepo-installer-3.8.1.jar /tmp/install.properties
+  then
     echo "Failed a second time to install from the Fedora jar... Can't proceed!"
     exit 1
   else
